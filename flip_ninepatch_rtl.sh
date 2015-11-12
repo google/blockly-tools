69f4843021ec5fee2e4c0fb1588dab5b4c415394
Torsten Rohlfing <rohlfing@google.com>
2015-11-11 17:02:32 -0800

    9-patches for block rendering.

---
+++ flip_ninepatch_rtl.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# This script takes as its input an existing 9-patch file for Left-to-Right
+# layouts and converts it into a 9-patch file for Right-to-Left mode. This
+# is done by horizontally flipping the image except the first and last columns,
+# which need to remain in place to encode vertical stretching and padding.
+#
+# The script requires ImageMagick (or GraphicsMagick) to be installed and
+# in the search path.
+
+input=$1
+output=$2
+
+if [ ! -f ${input} ]; then
+  echo "Input file ${input} does not exist."
+  exit 1
+fi
+
+if [ -f ${output} ]; then
+  echo "Output file ${output} already exists."
+  exit 1
+fi
+
+size_xy=$(identify ${input} | sed 's/.*\ \([0-9]\+x[0-9]\+\) .*/\1/')
+size_x=$(echo ${size_xy} | cut -dx -f1)
+size_y=$(echo ${size_xy} | cut -dx -f2)
+
+# Define the region specification for the entire image minus first and last
+# columns. For example, for a 25x16 image, this will be 23x16+1+0.
+region=$((size_x-2))x${size_y}+1+0
+
+# Apply horizontal flip to the selected image region.
+convert -region ${region} -flop ${input} ${output}
